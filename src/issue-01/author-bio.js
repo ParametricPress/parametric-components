35ff5cfcb051f4653238eb9d06dda95f79172205
Matthew Conlen <mc@mathisonian.com>
2019-04-10 02:52:50 -0700

    add author bio

---
+++ src/issue-01/author-bio.js
@@ -0,0 +1,18 @@
+const React = require('react');
+
+class CustomComponent extends React.Component {
+
+  componentDidCatch(e) {
+    console.log(e);
+  }
+  render() {
+    const { hasError, updateProps, children, ...props } = this.props;
+    return (
+      <div style={{fontFamily: 'Graphik Web', fontSize: '16px', fontWeigth: 600, lineHeight: 1.5, margin: '3em 0 2em' }}>
+        {children}
+      </div>
+    );
+  }
+}
+
+module.exports = CustomComponent;
