0687f91531b921b7cc6aefdb523dbe2d911704ec
Matthew Conlen <mc@mathisonian.com>
2020-09-20 16:07:12 -0700

    rebuild

--- src/issue-02/next-article.js
+++ src/issue-02/next-article.js
@@ -15,7 +15,7 @@ class CustomComponent extends React.Component {
     const { hasError, idyll, updateProps, ...props } = this.props;
     const article = this.getNextArticle();
     return (
-      <div className="parametric-recirc-next-article" style={{fontFamily: 'Graphik Web', fontWeight: 'bold', marginTop: '2em', marginBottom: 0, width: '100%', background: '#D8FFA2'}}>
+      <div className="parametric-recirc-next-article" style={{fontFamily: 'Graphik Web', fontWeight: 'bold', marginTop: '2em', marginBottom: 0, background: '#D8FFA2'}}>
         <a href={`/issue-02/${article.slug}/`} style={{fontSize: 56, lineHeight: '64px', fontWeight: 400, color: '#222222', textShadow: 'none', display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'space-between', padding: '1em 0', textDecoration: 'none'}}>
           <div>
             <span style={{fontWeight: 'bold'}}>Read the next article</span><br/>
