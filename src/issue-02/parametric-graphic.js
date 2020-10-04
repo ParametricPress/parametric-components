8f4b43db6d3afceff46b05655f3ac80ebd17ede9
Matthew Conlen <mc@mathisonian.com>
2020-10-04 15:35:24 -0700

    accept multiple sources, along with urls

--- src/issue-02/parametric-graphic.js
+++ src/issue-02/parametric-graphic.js
@@ -2,6 +2,47 @@ import React from 'react';
 
 class ParametricGraphic extends React.Component {
 
+  renderSource() {
+    let source = this.props.source || this.props.sources;
+
+    if (!source) {
+      return <div className="parametric-graphic-source-label-wrapper"></div>;
+    }
+
+    if (typeof source === 'string') {
+      return (
+        <div className="parametric-graphic-source-label-wrapper">
+          <div className="parametric-graphic-source-label">SOURCE</div>
+          <div className="parametric-graphic-source-citation">{props.source}</div>
+        </div>
+      )
+    }
+
+    if (!Array.isArray(source)) {
+      source = [source];
+    }
+
+    return (
+      <div className="parametric-graphic-source-label-wrapper">
+        <div className="parametric-graphic-source-label">SOURCE</div>
+        <div className="parametric-graphic-source-citation">
+          {source.map((s) => {
+            if (typeof s === 'string') {
+              return s;
+            }
+            return <a href={s.url}>{s.label}</a>
+          }).reduce((memo, el, idx, arr) => {
+            memo.push(el);
+            if (idx < arr.length - 1) {
+              memo.push(' ');
+            }
+            return memo;
+          }, [])}
+        </div>
+      </div>
+    )
+  }
+
   render() {
     const { hasError, idyll, updateProps, style, ...props } = this.props;
     return (
@@ -17,15 +58,12 @@ class ParametricGraphic extends React.Component {
 
         {props.children}
 
-        {props.source ? (
-          <div className="parametric-graphic-source">
-            <div className="parametric-graphic-source-label-wrapper">
-              <div className="parametric-graphic-source-label">SOURCE</div>
-              <div className="parametric-graphic-source-citation">{props.source}</div>
-            </div>
-            <div className="parametric-graphic-source-watermark">Parametric Press</div>
-          </div>
-        ) : (null)}
+        <div className="parametric-graphic-source">
+          {this.renderSource()}
+          <div className="parametric-graphic-source-watermark">Parametric Press</div>
+        </div>
+
+
 
       </div>
     );
