a7affbd12ea5f66776eb8b00a9d5d83e5e9b68d8
Matthew Conlen <mc@mathisonian.com>
2020-10-12 21:21:48 -0700

    update article descriptions

--- src/issue-02/articles.js
+++ src/issue-02/articles.js
@@ -4,31 +4,31 @@ const ARTICLES = [
     titleBreaks: ['Your Personalized', 'Carbon History'],
     author: 'Aatish Bhatia',
     slug: 'carbon-history',
-    description: 'TK',
+    description: 'Why we\'re living through an extraordinary time.',
   }, {
     title: '20 Companies Driving Climate Change',
     titleBreaks: ['20 Companies', 'Driving Climate Change'],
     author: 'Geoffrey Litt, Seth Thompson',
     slug: 'corporations',
-    description: 'TK'
+    description: 'The fossil fuels companies behind climate change.'
   }, {
     title: 'Algae: The Tiny CO₂ Killer',
     titleBreaks: ['Algae: The Tiny CO₂ Killer'],
     author: 'Benjamin Cooley',
     slug: 'algae',
-    description: 'TK',
+    description: 'To fight climate change, it’s time to start thinking big by thinking small.',
   }, {
     title: 'Drought of the Sinking Delta',
     titleBreaks: ['Drought of the Sinking Delta'],
     author: 'Christina Orieschnig',
     slug: 'sinking-delta',
-    description: "TK",
+    description: "Things are out of joint in the delta of the Mekong, Southeast Asia’s largest river.",
   }, {
     title: 'The Hidden Cost of Digital Consumption',
     titleBreaks: ['The Hidden Cost of Digital Consumption'],
     author: 'Halden Lin, Aishwarya Nirmal, Shobhit Hathi, Lilian Liang',
     slug: 'streaming',
-    description: 'TK',
+    description: 'Digital doesn\'t mean green.',
   }]
 
 module.exports = ARTICLES;
